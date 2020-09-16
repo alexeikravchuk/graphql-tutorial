7974619e3af54e8b890384d9557fbd3506b42c4f
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-09-16 17:06:59 +0300

    fix: used my mongodb url

--- server/server/app.js
+++ server/server/app.js
@@ -1,22 +1,28 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
 
-mongoose.connect('mongodb://Yauhen:Pass123@ds163835.mlab.com:63835/graphql-tutorial', { useMongoClient: true });
+mongoose.connect(
+  'mongodb+srv://Alexei:Pass123@geaphgl-tutorial.tfopi.mongodb.net/graphql-tutorial?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true,
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 const dbConnection = mongoose.connection;
-dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
 dbConnection.once('open', () => console.log('Connected to DB!'));
 
-app.listen(PORT, err => {
+app.listen(PORT, (err) => {
   err ? console.log(err) : console.log('Server started!');
 });
